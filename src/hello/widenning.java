9065f99d9e190b46c48745968d35ac102e6bbd47
testing <pragna.diwan@globalgarner.com>
2021-12-24 14:35:43 +0530

    selenium added

---
+++ src/hello/widenning.java
@@ -0,0 +1,19 @@
+package hello;
+
+public class widenning {
+
+	public static void main(String[] args) {
+		
+				double a=5;
+				float b=(float)a;
+				
+				System.out.println(a);
+				System.out.println(b);
+			}
+
+		
+
+
+	}
+
+
